00cd1cc4171d9635b2e00a5200cecc431476ed15
wangjunli <18235185436@163.com>
2019-04-09 21:26:22 +0800

    :sunglasses: add koa middleware test case

--- test/koa.test.js
+++ test/koa.test.js
@@ -1,7 +1,13 @@
 const Acid = require('../src/acid')
-
+const request = require('supertest')
 const Koa = require('koa')
-const app = new Koa()
 
-app.use(Acid.middleware())
-
+test('koa middleware', (done) => {
+    const app = new Koa()
+    app.use(Acid.middleware())
+    app.use((ctx) => {
+        expect(typeof ctx.acid).toEqual('function')
+        ctx.status = 200
+    })
+    request(app.listen()).get('/').expect(200, done)
+})
