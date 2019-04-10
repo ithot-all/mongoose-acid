e32f5dbc4b6f5d583b87263b59489d66d5a4c0a4
dtboy <936776640@qq.com>
2019-04-10 18:32:04 +0800

    :sunglasses: koa middleware fix test case

--- test/koa.test.js
+++ test/koa.test.js
@@ -1,5 +1,5 @@
 const Acid = require('../src/acid')
-const request = require('supertest')
+const request = require('request-promise')
 const Koa = require('koa')
 const app = new Koa()
 
@@ -12,11 +12,32 @@ app.use(async (ctx) => {
     }
 })
 
-test('koa middleware', (done) => {
-    request(app.listen())
-        .get('/')
-        .expect(200, {
-            exist: true,
-            type: 'function'
-        }, done)
+let server
+
+beforeAll((done) => {
+    server = app.listen(3333, (err) => {
+        if (!err) {
+            console.log('listen in 3333')
+        }
+        done()
+    })
+})
+afterAll((done) => {
+    server.close((err) => {
+        if (!err) {
+            console.log('server closed')
+        }
+        done()
+    })
+})
+
+test('koa middleware', async () => {
+    let data = await request({
+        uri: 'http://localhost:3333',
+        json: true
+    })
+    expect(data).toEqual({
+        exist: true,
+        type: 'function'
+    })
 })
